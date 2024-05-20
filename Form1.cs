254ead6c26f9c7de236aaa8cef8df141e112456f
mykolasmontv <mykolasmontvila420@gmail.com>
2024-05-20 21:17:30 +0300

    commitas

--- Form1.cs
+++ Form1.cs
@@ -1,13 +1,49 @@
+using System.Collections;
+
 namespace WinFormsApp3
 {
     public partial class Form1 : Form
     {
+        Random rand;
+        enum zaidimolangeliai
+        {
+            tuscias,
+            gyvate,
+            obuolys
+        };
+        enum kryptys
+        {
+            virs,
+            apac,
+            kair,
+            des
+        };
+
+        struct gyvateskoordinates
+        {
+            public int x;
+            public int y;
+        };
+
+        zaidimolangeliai[,] zaidimolangelis;
+        gyvateskoordinates[] gyvatexy;
+        int gyvatesilgis;
+        ArrangeDirection kryptis;
+        Graphics g;
         public Form1()
         {
             InitializeComponent();
+            zaidimolangelis = new zaidimolangeliai[11, 11];
+            gyvatexy = new gyvateskoordinates[100];
+            Random = new Random();
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
